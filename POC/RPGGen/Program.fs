7c42ea2263139a6315a4ee3862dfa01fcebd814b
Ryan Palmer <ryanpalmermultimedia@gmail.com>
2026-01-08 19:14:09 +0000

    Dedicated illustration agent

--- POC/RPGGen/Program.fs
+++ POC/RPGGen/Program.fs
@@ -3,6 +3,7 @@ open OpenAI
 open OpenAI.Responses
 open System.ClientModel
 open ImageGen
+open Microsoft.Agents.AI
 
 let runCliCmd (cmd : string) =
     let proc = new System.Diagnostics.Process()
@@ -34,6 +35,11 @@ let responseAgent =
     |> chatClient.GetResponsesClient
     |> _.CreateAIAgent(instructions = "", tools = [||])
 
+let getIllustrationDescription (thread : AgentThread) =
+    responseAgent.RunAsync($"The previous thread details a dungeons and dragons campaign. Describe the current scene such that an image generator can illustrate it to a high level of precision. Don't add any reply or commentary, just the scene description.", thread)
+    |> Async.AwaitTask
+    |> Async.RunSynchronously
+
 let storyThread = responseAgent.GetNewThread()
 
 let initPrompt = "Describe a classic scene from Dungeons and Dragons as if you are the dungeon master talking to the players. Don't describe your personal actions, just your words as the dungeon master."
@@ -45,8 +51,11 @@ let initialScene =
 
 Console.Write($"{initialScene.Text}\n\nIllustrating...\n\n")
 
+let illustrationDescription = getIllustrationDescription storyThread 
+Console.WriteLine $"Illustration description:\n\n{illustrationDescription.Text}\n\n\n"
+
 unloadLLM ()
-illustrateScene initialScene.Text |> ignore
+illustrateScene illustrationDescription.Text |> ignore
 
 while true do
     Console.Write("Enter the players' action:\n\n")
@@ -56,8 +65,10 @@ while true do
         responseAgent.RunAsync($"The players take the following action: {userAction}\n\nAs the dungeon master, describe what happens next.", storyThread)
         |> Async.AwaitTask
         |> Async.RunSynchronously
-    Console.WriteLine $"{dmResponse.Text}Illustrating...\n\n\n"
+    Console.WriteLine $"{dmResponse.Text}\n\n\nIllustrating...\n\n\n"
+    let illustrationDescription = getIllustrationDescription storyThread 
+    Console.WriteLine $"Illustration description:\n\n{illustrationDescription.Text}\n\n\n"
     unloadLLM ()
-    illustrateScene dmResponse.Text |> ignore
+    illustrateScene illustrationDescription.Text |> ignore
 
 
