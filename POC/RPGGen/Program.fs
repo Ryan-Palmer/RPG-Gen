e884de8c66e5f13aba9646723d664f8b46090881
Ryan Palmer <ryanpalmermultimedia@gmail.com>
2026-01-08 21:13:24 +0000

    tidy

--- POC/RPGGen/Program.fs
+++ POC/RPGGen/Program.fs
@@ -2,6 +2,7 @@
 open OpenAI
 open OpenAI.Responses
 open System.ClientModel
+open System.Diagnostics
 
 let llmServerRoot = "http://localhost:1234/v1"
 let storyModel = "gemma-3-27b-it-qat"
@@ -17,37 +18,81 @@ let responseAgent =
     |> chatClient.GetResponsesClient
     |> _.CreateAIAgent(instructions = "", tools = [||])
 
+let stopwatch = Stopwatch()
+
 let game = async {
-    Console.WriteLine("RPGGen initialising...\n\n")
+    Console.WriteLine("RPGGen initialising...\n")
+    stopwatch.Start()
     
     let storyThread = responseAgent.GetNewThread()
     let! initialScene = DungeonMaster.getInitialScene storyThread responseAgent
 
-    Console.Write($"{initialScene.Text}\n\nIllustrating...\n\n")
+    stopwatch.Stop()
+    let initTurnSeconds = stopwatch.Elapsed.Seconds
+    Console.WriteLine($"Initial scene generated in {initTurnSeconds} seconds\n\n")
+    stopwatch.Reset()
+    stopwatch.Start()
 
+    Console.WriteLine($"{initialScene.Text}\n\nIllustrating...\n")
+    
     let! illustrationDescription = Illustrator.getIllustrationDescription storyThread responseAgent
     //Console.WriteLine $"Illustration description:\n\n{illustrationDescription.Text}\n\n\n"
+
+    stopwatch.Stop()
+    let initIllustrationDescriptionSeconds = stopwatch.Elapsed.Seconds
+    Console.WriteLine($"Illustration description generated in {initIllustrationDescriptionSeconds} seconds\n")
+    stopwatch.Reset()
+    stopwatch.Start()
 
     do! unloadResponseAgent ()
 
     Illustrator.illustrateScene illustrationDescription.Text |> ignore
 
+    stopwatch.Stop()
+    let initIllustrationSeconds = stopwatch.Elapsed.Seconds
+    Console.WriteLine($"Illustration generated in {initIllustrationSeconds} seconds\n")
+
+    Console.WriteLine($"Total init time {initTurnSeconds + initIllustrationDescriptionSeconds + initIllustrationSeconds} seconds\n\n")
+
     while true do
-        Console.Write("Enter the players' action:\n\n")
+        stopwatch.Reset()
+        stopwatch.Start()
+
+        Console.WriteLine("Enter the players' action:\n")
         let userAction = Console.ReadLine()
 
-        Console.Write("\n\nGenerating...\n\n\n")
+        Console.WriteLine("\n\nGenerating...\n")
         let! dmResponse = DungeonMaster.takeTurn storyThread responseAgent userAction
+        
+        stopwatch.Stop()
+        let turnSeconds = stopwatch.Elapsed.Seconds
+        Console.WriteLine($"Turn generated in {turnSeconds} seconds\n")
+        stopwatch.Reset()
+        stopwatch.Start()
 
-        Console.WriteLine $"{dmResponse.Text}\n\n\nIllustrating...\n\n\n"
+        Console.WriteLine $"{dmResponse.Text}\n\n\nIllustrating...\n"
 
         let! illustrationDescription = Illustrator.getIllustrationDescription storyThread responseAgent
         //Console.WriteLine $"Illustration description:\n\n{illustrationDescription.Text}\n\n\n"
 
+        stopwatch.Stop()
+        let illustrationDescriptionSeconds = stopwatch.Elapsed.Seconds
+        Console.WriteLine($"Illustration description generated in {illustrationDescriptionSeconds} seconds\n")
+        stopwatch.Reset()
+        stopwatch.Start()
+
         do! unloadResponseAgent ()
 
         Illustrator.illustrateScene illustrationDescription.Text |> ignore
+
+        stopwatch.Stop()
+        let illustrationSeconds = stopwatch.Elapsed.Seconds
+        Console.WriteLine($"Illustration generated in {illustrationSeconds} seconds\n\n")
+        stopwatch.Reset()
+        stopwatch.Start()
+
+        Console.WriteLine($"Total turn time {turnSeconds + illustrationDescriptionSeconds + illustrationSeconds} seconds\n\n")
 }
 
 game
- |> Async.RunSynchronously
+|> Async.RunSynchronously
