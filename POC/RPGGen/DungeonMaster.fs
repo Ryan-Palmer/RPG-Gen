7f1641cdb69117b08d6e98fa1de14f9821815345
Ryan Palmer <ryanpalmermultimedia@gmail.com>
2026-01-08 22:17:54 +0000

    Be creative!

--- POC/RPGGen/DungeonMaster.fs
+++ POC/RPGGen/DungeonMaster.fs
@@ -2,7 +2,7 @@ module DungeonMaster
 
 open Microsoft.Agents.AI
 
-let initPrompt = "Describe a classic scene from Dungeons and Dragons as if you are the dungeon master talking to the players. Don't describe your personal actions, just your words as the dungeon master."
+let initPrompt = "Describe a classic scene from Dungeons and Dragons as if you are the dungeon master talking to the players. Be super creative, let your imagination run wild. Don't describe your personal actions, just your words as the dungeon master."
 
 let getInitialScene (thread : AgentThread) (agent : ChatClientAgent) =
     agent.RunAsync(initPrompt, thread)
